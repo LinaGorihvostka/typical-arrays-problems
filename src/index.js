14521eb2eecd9415590951c806a154120d010389
Lina Gorihvostka <lina.editor@gmail.com>
2021-03-15 18:13:55 +0300

    init: start typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+
+  let isArray = Array.isArray( array );
+
+  if (!(isArray && array.length !== 0)) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+
+  let isArray = Array.isArray( array );
+
+  if (!(isArray && array.length !== 0)) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+
+  let isArray = Array.isArray( array );
+  let sumOfElements = 0;
+  let averageValue = 0;
+
+  if (!(isArray && array.length !== 0)) {
+    return 0;
+  } else {
+    sumOfElements = array.reduce(function(sum, element) {
+      return sum + element;
+    }, 0);
+    averageValue = sumOfElements / array.length;
+    return averageValue;
+  }
+};
